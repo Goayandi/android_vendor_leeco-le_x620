441eb30e649cd26c0d20bcf372faa2a39fdf5373
graineeg <graineeg@gmail.com>
2017-09-08 22:42:39 +0200

    update

--- Android.mk
+++ Android.mk
@@ -2,10 +2,30 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),le_x620)
 
+ifeq ($(USE_MTK_CAMERA_WRAPPER),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam2halsensor
+LOCAL_SRC_FILES_64 := wrapper/lib64/libcam2halsensor.so
+LOCAL_SRC_FILES_32 := wrapper/lib/libcam2halsensor.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcameracustom
-LOCAL_SRC_FILES_64 := system/lib64/libcameracustom.so
-LOCAL_SRC_FILES_32 := system/lib/libcameracustom.so
+LOCAL_SRC_FILES_64 := wrapper/lib64/libcameracustom.so
+LOCAL_SRC_FILES_32 := wrapper/lib/libcameracustom.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmtkcamera_client
+LOCAL_SRC_FILES_64 := system/lib64/libmtkcamera_client.so
+LOCAL_SRC_FILES_32 := system/lib/libmtkcamera_client.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
